eec0fb69916aa04984cd7f6c96c03c1ac767e772
olivermen69 <olivermen69@hotmail.com>
2019-12-10 22:23:05 -0500

    Clase6_PlayerMoviemiento2D

---
+++ Clase_5/Area51_2192-master/VisualStudio/MyHangManGame/MyHangManGame/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyHangManGame
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            /*
+            string secretWord = "zapatos";
+            string publicWord = "";
+            string tempWord = "";
+            string letter = "";
+            int hp = 3;
+            
+
+            for (int i = 0; i < secretWord.Length; i++) {
+                tempWord += "*";
+            }
+            publicWord = tempWord;
+            Console.WriteLine(publicWord);
+
+
+            while (hp > 0) {
+                Console.WriteLine("HP = " + hp);
+                Console.WriteLine("Ingrese una letra:");
+                letter = Console.ReadLine();
+                Console.Clear();
+
+                tempWord = "";
+                for (int i = 0; i < secretWord.Length; i++) {
+                    if (letter == "" + secretWord[i]) {
+                        tempWord += letter; //tempWord += secretWord[i];
+                    }
+                    else {
+                        tempWord += publicWord[i];
+                    }
+                }
+
+                if (publicWord == tempWord) {
+                    hp--;
+                }
+
+                if(secretWord == tempWord) {
+                    Console.WriteLine("YOU WIN!!");
+                    break;
+                }
+
+                publicWord = tempWord;
+                Console.WriteLine(publicWord);
+            }
+
+            if (hp <= 0) {
+                Console.WriteLine("GAME OVER");
+            }
+            
+            Console.ReadLine();
+            */
+
+            GameManager gameManager = new GameManager();
+            Player player = new Player();
+            Board board = new Board();
+
+            gameManager.SetSecretWord("lapTop");
+
+            while(gameManager.isPlaying) {
+                board.Clear();
+                board.Draw(player.Life());
+
+                // OPTION 2
+                if (player.IsDead()) {
+                    board.Draw("GAME OVER!!");
+                    board.StarWars();
+                    break;
+                }
+                    
+
+                board.Draw(gameManager.publicWord);
+
+                string letter = player.EnterWord();
+                if (gameManager.CheckLetter(letter))
+                    gameManager.UpdatePublicWord(letter);
+                else
+                    player.Damage();
+
+                // OPTION 1
+                //if (player.IsDead())
+                //    gameManager.isPlaying = false;
+
+                if (gameManager.IsWin())
+                    gameManager.isPlaying = false;
+
+            }
+            // OPTION 1
+            //board.Clear();
+            //board.Draw(player.Life());
+            //board.Draw("GAME OVER!!");
+
+            if (gameManager.IsWin()) {
+                board.Draw("YOU WIN!!");
+                board.Mario();
+            }
+
+
+
+            board.Close();
+        }
+    }
+}
---
+++ Clase_6/Proyecto_practica/Assets/APP/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// CODIGO PARA MOVER AL PERSONAJE EN VECTOR 3
+
+public class Player : MonoBehaviour
+{
+
+
+    public float speed = 1f;
+
+    public Animator animator;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rigidbody2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2 = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log(Input.GetAxis("Horizontal"));
+
+        float h = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+
+        if (h != 0)
+            animator.SetBool("Walk", true);
+        else
+            animator.SetBool("Walk", false);
+
+
+        // Es otra forma de nombrar al sprite de que se mueve o no
+        // animator.SetBool("walk", (h != 0));
+
+
+
+
+        // CODIGO PARA VOLTEAR EL SPRITE SHEET AL EJE -X DEL X
+        if (h > 0)
+            spriteRenderer.flipX = false;
+
+        else if (h < 0)
+            spriteRenderer.flipX = true;
+
+        //if (h < 0)
+        // spriteRenderer.flipX = true;
+
+
+
+
+        //translate siempre requiere un Vector3
+        transform.Translate(Vector3.right * h * speed);
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        rigidbody2.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+
+
+
+    }
+
+ 
+
+        
+
+         }
+
+
+
+
+
